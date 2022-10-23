ddb45611cd52687e12b10a7d3b02bb8170d35dc8
ECleverito <eclever12@gmail.com>
2022-10-23 05:53:19 -0600

    Adding aesd-char-driver to buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,12 +5,16 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '73665f3d03655544cf730d44677e1a0e6cd9ed07'
+AESD_ASSIGNMENTS_VERSION = '4674fa61aff4505c61330e2e3474bd98351a746c'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ECleverito.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+AESD_ASSIGNMENTS_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE)
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
@@ -24,7 +28,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
 
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
