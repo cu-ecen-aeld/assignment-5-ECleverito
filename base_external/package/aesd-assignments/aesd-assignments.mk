7b3e7c6969194f3d6edcde9853c597bf16056624
ECleverito <eclever12@gmail.com>
2022-10-25 03:02:02 -0600

    Have had success with running drivertest.h using my source on buildroot up to this point. Now updating aesdsocket app.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '15300b7d8454cd06e0f84f3f50009bd3a0cd5816'
+AESD_ASSIGNMENTS_VERSION = '5d5b0b14def82e67b3c7ceb7f1b907fd3e81d4e4'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ECleverito.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,21 +5,17 @@
 #
 ##############################################################
 
-AESDCHAR_VERSION = '17df0d4bb15b2ca86cd9cb1ff4a17806c7247f3d'
+$(eval $(kernel-module))
+AESDCHAR_VERSION = '0a287a3c35c53a0dba61a74192bd2abaac184b3d'
 AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ECleverito.git'
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
 
 AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 
-#define AESDCHAR_BUILD_CMDS
-	#$(MAKE) 
-#endef
-
 define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 endef
 
-$(eval $(kernel-module))
 $(eval $(generic-package))
